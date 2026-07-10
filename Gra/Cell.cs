fd886e2eda66aa23e5acc40ebf77b2ddd516a597
yournightmarexdemon <sandi525@interia.pl>
2026-07-10 18:21:58 +0200

    add map structure

---
+++ Gra/Cell.cs
@@ -0,0 +1,28 @@
+namespace ClassesIntro;
+
+public class Cell
+{
+    private readonly char _terrainSymbol;
+
+    public Item? ItemOnCell { get; set; }
+
+    public bool IsWalkable => _terrainSymbol != '#';
+
+    public Cell(char terrainSymbol)
+    {
+        _terrainSymbol = terrainSymbol;
+    }
+
+    public void Display()
+    {
+        // Przedmiot leżący na ziemi jest zawsze rysowany "na wierzchu" terenu.
+        if (ItemOnCell != null)
+        {
+            GameColors.WriteColored(ItemOnCell.Symbol, ItemOnCell.Color);
+            return;
+        }
+
+        ConsoleColor color = _terrainSymbol == '#' ? GameColors.Wall : GameColors.Floor;
+        GameColors.WriteColored(_terrainSymbol, color);
+    }
+}
